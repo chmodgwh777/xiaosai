6e69452ee95470e502b7392567aa68b77c50eb37
chmodgwh777 <646525033@qq.com>
2019-04-29 23:17:23 +0800

    插值完毕，可以运行

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy as np
 from scipy.fftpack import fft,ifft
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
-from scipy.interpolate import interp1d
 import platform
 
 if platform.system() == 'Darwin':
@@ -24,12 +23,12 @@ for i in range(30):
 vMean1 = accumulate1 / time
 # extract data1 end
 
-t = np.linspace(0, 1, 30)
-print(t)
-
-f = interp1d(t, data1, kind='cubic')
+vMean1 = np.concatenate((vMean1[0:1], vMean1))
+f = interp1d(np.linspace(0, 1, 31), vMean1, kind='cubic')
+vInter = [f(t) for t in np.linspace(0, 1, 121)] # len(vInter) == 30k+1, where k is an integer
 
 # fft
+N = len(vInter)
 vfft = fft(vInter)
 vMod = abs(vfft)
 vMod /= (N/2)
@@ -51,10 +50,13 @@ plot2 = 0
 plot1 = 1
 plot2 = 1
 if plot1:
-    t1 = np.linspace(0, 1, 30)
-    plt.plot(t1, vMean1)
+    t1 = np.linspace(0, 1, 31)
+    plt.plot(t1, vMean1, 'r', label='rawData')
 if plot2:
     t2 = np.linspace(0, 1, 1000)
-    plt.plot(t2, list(map(lambda t: result(t, 1.04), t2)))
-    plt.plot(t2, list(map(lambda t: result(t, 0), t2)))
+    th1 = 0
+    th2 = 1.04
+    plt.plot(t2, [result(ti, th1) for ti in t2], 'b', label='threshold=%f'%th1)
+    plt.plot(t2, [result(ti, th2) for ti in t2], 'g', label='threshold=%f'%th2)
+plt.legend()
 plt.show()
